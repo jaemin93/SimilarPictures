317fee09e8e52ec629c06e60aac03779de88bf4e
KimHyungIlko <hi465465@naver.com>
2019-02-12 20:11:58 +0900

    dif clustering test

--- make_labels_pred.py
+++ make_labels_pred.py
@@ -3,12 +3,14 @@
 """
 import os
 import numpy as np
-from sklearn.cluster import KMeans
+from sklearn.cluster import KMeans, DBSCAN
+from scipy.cluster.hierarchy import linkage, dendrogram, fcluster
+import matplotlib.pyplot as plt
 from sklearn import metrics
 from config import *
-from feature_mapping import pred_num_cluster
 
-def make_labels_pred(number_of_k):
+
+def make_labels_pred():
     """
     K-Means 알고리즘으로 특징 벡터를 클러스터링 하는 함수입니다.
     예측 레이블은 DATA_DIR/LABELS_PRED.npy 에 저장됩니다.
@@ -16,18 +18,22 @@ def make_labels_pred(number_of_k):
     """
     # load datasets
     features = np.load(os.path.join(DATA_DIR, FEATURES + ".npy"))
-    print(number_of_k)
+
     # estimate number of clusters
-    if int(number_of_k) == -1:
-        NUM_IMGS_PER_MODEL = pred_num_cluster._main()
-    else:
-        NUM_IMGS_PER_MODEL = int(number_of_k)
     num_clusters = int(len(features)/NUM_IMGS_PER_MODEL)
     print("Estimated num_clusters: %d" % num_clusters)
 
     # make prediction
-    labels_pred = KMeans(n_clusters=4, verbose=0).fit_predict(features)
-
+    '''kmeans 구조
+    labels_pred = KMeans(n_clusters=num_clusters, verbose=0).fit_predict(features)
+    '''
+    '''계층 구조
+    mergings = linkage(features, method='complete')
+    labels_pred = fcluster(mergings, 153, criterion='distance').fit_predict(features)
+    '''
+    '''밀도 구조 eps는 점중심에서의 거리안에 min_samples의 수만큼 모여있으면 군집형성
+    labels_pred = DBSCAN(eps=0.3,min_samples=num_clusters).fit_predict(features)
+    '''
     # save predicted labels
     np.save(os.path.join(DATA_DIR, LABELS_PRED + ".npy"), labels_pred)
     np.savetxt(os.path.join(DATA_DIR, LABELS_PRED + ".tsv"), labels_pred, "%d", delimiter="\t")
@@ -35,4 +41,4 @@ def make_labels_pred(number_of_k):
 
 
 if __name__ == '__main__':
-    make_labels_pred(number_of_k)
+    make_labels_pred()
